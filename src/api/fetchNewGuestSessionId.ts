4406f86509e0f249e74514f9d451630020bcb842
fichorka <fichorka@gmail.com>
2020-09-26 21:55:33 +0200

    Add Api function: fetchNewGuestSessionId

---
+++ src/api/fetchNewGuestSessionId.ts
@@ -0,0 +1,25 @@
+import { API_BASE_URL, API_KEY } from '../../config'
+
+const urlEndpoint = `${API_BASE_URL}authentication/guest_session/new`
+
+const fetchNewGuestSessionId: FetchNewGuestSessionId = async function () {
+  const requestUrl = urlEndpoint + `?${API_KEY}`
+  return await fetch(requestUrl)
+    .then((res) => res.json())
+    .then((res: NewGuestSessionResponse) => {
+      if (!res.success || !res.guest_session_id) throw new Error()
+      return res.guest_session_id
+    })
+    .catch(() => {
+      throw new Error('Failed to fetch a new guest session id.')
+    })
+}
+
+export { fetchNewGuestSessionId }
+
+type FetchNewGuestSessionId = () => Promise<string>
+
+interface NewGuestSessionResponse {
+  success?: boolean
+  guest_session_id?: string
+}
--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,4 @@
 export * from './fetchMovieList'
 export * from './fetchGenres'
 export * from './fetchMovieDetails'
+export * from './fetchNewGuestSessionId'
