be30de538ac4a53f6a91ec3309ecd3ba86229c4d
fichorka <fichorka@gmail.com>
2020-09-26 20:29:03 +0200

    Add API function: fetchGenres

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { fetchGenres } from './api/fetchGenres'
 import { fetchMovieList } from './api/fetchMovieList'
 
 export const App: React.FC = () => {
-  fetchMovieList({}).then((res) => console.log(res))
+  fetchGenres().then((res) => console.log(res))
   return (
     <div>
       <h1>Hello</h1>
---
+++ src/api/fetchGenres.ts
@@ -0,0 +1,22 @@
+import { API_BASE_URL, API_KEY } from '../../config'
+
+const endpointUrl =
+  API_BASE_URL + `genre/movie/list?api_key=${API_KEY}&language=en-US`
+
+const fetchGenres: FetchGenres = async function () {
+  return await fetch(endpointUrl)
+    .then((res) => res.json())
+    .then((data) => data.genres)
+    .catch(() => {
+      throw new Error('Failed to fetch the genre list.')
+    })
+}
+
+export { fetchGenres }
+
+type FetchGenres = () => Promise<Genre[]>
+
+interface Genre {
+  id: number
+  name: string
+}
